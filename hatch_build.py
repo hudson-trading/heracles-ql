7a23c7f5cb1c383ab243cf813ef6513d0147ed1f
Ethan Hunter <ehunter@hudson-trading.com>
2025-10-02 13:55:48 -0600

    add github actions workflow for publishing to pypi (#2)

--- hatch_build.py
+++ hatch_build.py
@@ -6,6 +6,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 class BuildSharedObjectd(BuildHookInterface):
     def initialize(self, version, build_data):
         build_data["pure_python"] = False
-        build_data["tag"] = "py3-none-linux_x86_64"
 
         subprocess.check_output(["make", "format-lib"])
